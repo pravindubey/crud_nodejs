bb0cbcc19fe96e0f5894a34f883cd1840f9f771d
pravindubey <59089025+pravindubey@users.noreply.github.com>
2020-12-31 12:17:38 +0530

    project in progress, update route added

--- routes/index.js
+++ routes/index.js
@@ -68,6 +68,77 @@ router.post('/addUser', function(req, res, next) {
     })
  })
 
-})
+
+});
+
+router.post('/updateUser', function(req, res, next) {
+
+  const empNo = req.params.empNo;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  var momentDob = moment(req.body.dob, 'DD-MM-YYYY');
+  const Dob = moment(momentDob).format("YYYY-MM-DD");
+  console.log(Dob)
+  const city = req.body.city;
+  const mobileNo = req.body.mobileNo;
+  const errors = false;
+
+  if(firstName.length === 0 || lastName.length === 0 || DOB.length === 0 || city.length === 0 || mobileNo.length == 0) {
+      errors = true;
+      
+      // set flash message
+      req.json('error', "Please enter firstNmae,lastName,DOB,city and mobileNo");
+      // render to add.ejs with flash message
+      res.json('emp_data/edit', {
+          emoNo: req.params.empNo,
+          firstName: firstName,
+          lastName: lastName,
+          DOB:DOB,
+          city:city,
+          mobileNO:mobileNo
+
+      })
+  }
+
+  // if no error
+  if( !errors ) {   
+
+      var data = {
+        firstName: firstName,
+        lastName: lastName,
+        Dob:Dob,
+        city:city,
+        mobileNO:mobileNo
+      }
+      // update query
+      connection.query('UPDATE emp_data SET ? WHERE empNo = ' + No, data, function(err, result) {
+          //if(err) throw err
+          if (err)  {
+            res.json({
+              success: false,
+              msg: error
+            })
+          }
+              // render to edit.ejs
+              res.json({
+                success: true,
+                msg: "Data updated"
+              })
+            }
+        
+
+              else {
+            res.json({
+              msg: "not updated"
+            })
+            }
+          })    
+  
+});
+
+
+
+
+
 
 module.exports = router;
